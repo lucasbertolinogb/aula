171bb1bca22a22e069895ba422d2c2754f750adf
lucasbertolinogb <102763228+lucasbertolinogb@users.noreply.github.com>
2023-08-10 08:56:47 -0300

    Create Main.java

---
+++ códigos/Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+
+    Cliente lucas = new Cliente();
+    Veiculo Gol = new Veiculo();
+    Seguro Protectus = new Seguro();
+    
+
+    
+    lucas.name = "Lucas";
+    lucas.cpf = 123456789;
+    
+  }
+}
